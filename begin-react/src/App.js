c423e19b38f041f8682e4daf24ea5935c2d4d7e3
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-10-22 23:56:52 +0900

    backbone

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import Hello from './Hello';
 import Wrapper from './Wrapper';
-
+import Counter from './Counter';
 
 function App() {
-  const name = 'react';
-  const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: 24,
-    padding: '1rem',
-  };
+
 
   return (
-    <Wrapper>
-      <Hello name='react'color ='red' isSpecial={true} />
-      <Hello color ='pink' />
-    </Wrapper>
+    <Counter />
   );
 }
 
