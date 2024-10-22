439b604aad51488f3c9e9b89ce12fb5ee7c20852
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-10-22 23:50:15 +0900

    conditional rendering

--- begin-react/src/Hello.js
+++ begin-react/src/Hello.js
@@ -1,12 +1,16 @@
 import React from "react";
 
-function Hello({ color, name }) {
-  return <div style={{ color: color }}>안녕하세요 {name}</div>;
+function Hello({ color, name, isSpecial }) {
+  return (
+    <div style={{ color: color }}>
+      {isSpecial ? <b>*</b> : null}
+      안녕하세요 {name}
+    </div>
+  );
 }
 Hello.defaultProps = {
   name: "이름없음",
 };
 
 export default Hello;
-// test 
-
+// test
