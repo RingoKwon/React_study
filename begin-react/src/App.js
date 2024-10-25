94b3bb0da28b53371f77b89b5943d31e03092fd6
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-10-25 17:32:26 +0900

    map

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import InputSample from "./InputSample";
-
+import UserList from "./UserList";
 function App() {
   return (
     <div>
       <h1>인풋 샘플</h1>
       <InputSample />
+      <h1>유저 목록</h1>
+      <UserList />
     </div>
   );
 }
