ed68f5274b745a61f22dbf08af76d885b22cd16d
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-10-25 22:17:26 +0900

    save

--- begin-react/src/UserList.js
+++ begin-react/src/UserList.js
@@ -1,38 +1,20 @@
 import React from "react";
 
-function UserList() {
-
-    function User({user}) {
-        return (
-            <div>
-                <b>{user.username}</b> <span>{user.email}</span>
-            </div>
-        );
-    }
-    const users = [
-        {
-            id: 1,
-            username: 'velopert',
-            email: 'public.velopert@gmail.com'
-        },
-        {
-            id: 2,
-            username: 'tester',
-            email: 'tester@example.com'
-        },
-        {
-            id: 3,
-            username: 'liz',
-            email: 'liz@example.com'
-        },
-    ];
-    return (
-        <div>
-           {users.map(user => (
-            <User user={user}  />
-           ))}   
-        </div>
-    );
+function User({ user }) {
+  return (
+    <div>
+      <b>{user.username}</b> <span>{user.email}</span>
+    </div>
+  );
+}
+function UserList({ users }) {
+  return (
+    <div>
+      {users.map((user) => (
+        <User user={user} key={user.id} />
+      ))}
+    </div>
+  );
 }
 
 export default UserList;
