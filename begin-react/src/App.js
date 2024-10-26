a18ca6ebada128a3513b691c103a459a2a840837
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-10-26 16:48:55 +0900

    save

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,8 +1,12 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useMemo } from "react";
 // import InputSample from "./InputSample";
 import UserList from "./UserList";
 import CreateUser from "./CreateUser";
 
+function countActiveUsers(users) {
+  console.log("활성 사용자 수를 세는 중");
+  return users.filter((user) => user.active).length;
+}
 function App() {
   const [inputs, setInputs] = useState({
     username: "",
@@ -67,6 +71,7 @@ function App() {
     );
   };
 
+  const count = useMemo(() => countActiveUsers(users), [users]);
   return (
     <>
       <CreateUser
@@ -76,6 +81,7 @@ function App() {
         onCreate={onCreate}
       />
       <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+      <div>활성 사용자 수 : {count}</div>
     </>
   );
 }
